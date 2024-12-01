6e2aaf5ff5bdaa24a6cb2c89856051d432f92d10
Pritam Bhuin <143198447+detactivepritam@users.noreply.github.com>
2024-12-01 13:10:25 +0530

    Add files via upload
     I add  speech_recognition library  to listen for and recognize voice commands

--- Main.py
+++ Main.py
@@ -1,9 +1,32 @@
+import speech_recognition as sr
 import pyttsx3
+
 def speak(text):
-    engine = pyttsx3.init()  
+    engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
-    
 
 if __name__ == "__main__":
-    speak("How may I help you, Pritam?")
+    speak("Initializing Jarvis...")
+    r = sr.Recognizer()
+
+    while True:
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=5, phrase_time_limit=3)
+            
+            word = r.recognize_google(audio).lower()
+            print(word)
+            
+            # Respond to recognized words
+            if "hello" in word:
+                speak("Hello! How can I assist you?")
+            
+        
+        except sr.UnknownValueError:
+            print("Could not understand the audio. Please speak again.")
+        except sr.RequestError:
+            print("Network error. Check your internet connection.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
