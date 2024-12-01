26f2273cad499e1b4c663aa19e4735693af00eb6
Pritam Bhuin <143198447+detactivepritam@users.noreply.github.com>
2024-12-02 02:18:04 +0530

    Add files via upload
    I  add  webbrowser module  to Open websites like Google, Facebook, YouTube, and LinkedIn based on voice
    commands.

--- Main.py
+++ Main.py
@@ -1,10 +1,21 @@
 import speech_recognition as sr
 import pyttsx3
+import webbrowser 
 
 def speak(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+
+def processCommand(command):
+    if "open google" in command.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in command.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in command.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in command.lower():
+        webbrowser.open("https://linkedin.com")
 
 if __name__ == "__main__":
     speak("Initializing Jarvis...")
@@ -14,14 +25,17 @@ if __name__ == "__main__":
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source, timeout=5, phrase_time_limit=3)
+                audio = r.listen(source, timeout=5, phrase_time_limit=10)
             
             word = r.recognize_google(audio).lower()
             print(word)
             
-            # Respond to recognized words
+            
+            
             if "hello" in word:
                 speak("Hello! How can I assist you?")
+            else:
+                processCommand(word)
             
         
         except sr.UnknownValueError:
