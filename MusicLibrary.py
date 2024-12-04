ddb9a4a029ef7255aa394c70ef41b20721a577e4
detactivepritam <detactivepritam@gmail.com>
2024-12-04 21:10:35 +0530

    add songs in a file

---
+++ MusicLibrary.py
@@ -0,0 +1,4 @@
+songs = {"person": "https://youtu.be/nyq9r8OYmn0?si=qQ1C0MjHNZ2UKwqz",
+         "stay":"https://youtu.be/kTJczUoc26U?si=g9P44Mabm8WhfKAh",
+         "durga":"https://youtu.be/MsqpjM09MrY?si=-svweMoANaOOsimI",
+         "friend":"https://youtu.be/sZdmkif8CSY?si=nmoTLLtDGm4F9dGf"}
